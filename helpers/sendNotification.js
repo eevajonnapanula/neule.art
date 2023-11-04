ac024d4d958972b7481d80d54d0a52a3a8c89249
Eeva-Jonna Panula <eevajonna.panula@gmail.com>
2023-11-04 07:15:18 +0200

    Fix notification sending

--- helpers/sendNotification.js
+++ helpers/sendNotification.js
@@ -10,6 +10,12 @@ admin.initializeApp({
 })
 
 const sendPushNotification = topics => {
+  const topicsToBeUsed = topicsIntoChunks([...new Set(topics)])
+
+  topicsToBeUsed.forEach(topic => formConditionAndSendNotification(topic))
+}
+
+const formConditionAndSendNotification = topics => {
   const condition = topics.map(topic => `'${topic}' in topics`).join(' || ')
   const message = {
     condition,
@@ -20,6 +26,22 @@ const sendPushNotification = topics => {
     .send(message)
     .then(res => console.log(`Successful message: ${res}`))
     .catch(e => console.log(`error: ${e}`))
+}
+
+const topicsIntoChunks = topics => {
+  const chunkAmount = Math.ceil(topics.length / 5)
+  return getChunk(topics, [], chunkAmount)
+}
+
+const getChunk = (topics, chunks, times) => {
+  if (times == 1) {
+    const chunk = topics
+    return [...chunks, chunk]
+  } else {
+    const chunk = topics.slice(0, 5)
+    const nextTopics = topics.slice(5)
+    return getChunk(nextTopics, [...chunks, chunk], times - 1)
+  }
 }
 
 module.exports = {
